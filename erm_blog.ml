83d5be4a472e4cf2c332ef221700cbe7b49a996d
Anastasia Gornostaeva <ermine@ermine.pp.ru>
2011-05-19 18:48:34 +0400

    Initial commit

---
+++ erm_blog.ml
@@ -0,0 +1,22 @@
+open Lwt
+open Eliom_pervasives
+open HTML5.M
+open Eliom_parameters
+
+let site_title = "Ermine's Blog"
+
+let srv_home =
+  Eliom_services.service
+    ~path:[]
+    ~get_params:unit
+    ()
+
+let default_page content =
+  html (head (title (pcdata site_title)) [])
+    (body content)
+
+let srv_home_handler () () =
+  return (default_page [])
+
+let _ =
+  Eliom_output.Html5.register ~service:srv_home srv_home_handler
