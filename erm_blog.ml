12e58e6b7a471d41f59b3be5ee58a7469ac6c102
Anastasia Gornostaeva <ermine@ermine.pp.ru>
2011-05-22 00:36:02 +0400

    Simple visit page counter

--- erm_blog.ml
+++ erm_blog.ml
@@ -14,20 +14,37 @@ let srv_home =
     ~get_params:unit
     ()
 
+let visited_eref = Eliom_references.eref 0
+
+let visited_page =
+  let mutex = Lwt_mutex.create () in
+    fun () ->
+      Lwt_mutex.lock mutex >>= fun () ->
+      Eliom_references.get visited_eref >>= fun i ->
+      let j = succ i in
+        Eliom_references.set visited_eref j >>= fun () ->
+      Lwt_mutex.unlock mutex;
+        return j
+
 let default_page content =
   (Eliom_references.get users >>= function
     | Some user -> return [pcdata user;
                            logout_box ()]
     | None -> return [login_box ()]
   ) >>= fun login_box ->
-  return (html (head (title (pcdata site_title)) [
-    HTML5.M.link ~rel:[`Stylesheet]
-      ~href:(HTML5.M.uri_of_string"./css/style.css") ()
-  ])
+  visited_page () >>= fun v ->
+  return (html
+            (head (title (pcdata site_title)) [
+              HTML5.M.link ~rel:[`Stylesheet]
+                ~href:(HTML5.M.uri_of_string"./css/style.css") ()
+            ])
             (body [
               div ~a:[a_id "login"] login_box;
-      div ~a:[a_id "content"] content
-]))
+              div ~a:[a_id "content"] content;
+              div ~a:[a_id "footer"] [p [pcdata "(c) 2011 ermine"];
+                                      p [pcdata ("Просмотров: " ^ string_of_int v)]
+                                     ]
+            ]))
 
 let srv_home_handler () () =
   default_page [p [pcdata "Hello, world!"]]
